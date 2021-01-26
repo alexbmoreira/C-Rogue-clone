6a8ff4ee23f32e50d03d58a4fca228fc0b3ba569
alexbmoreira <alexbmoreira@gmail.com>
2021-01-26 10:22:56 -0500

    Creates 9 random sized rooms (all equal to each other for now)

--- a1.c
+++ a1.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 #include "graphics.h"
 
@@ -323,6 +324,87 @@ void mouse(int button, int state, int x, int y) {
 }
 
 
+int getRandom(int min, int max) {
+   srand(time(NULL));
+   int r = (rand() % (max + 1 - min)) + min;
+   return r;
+}
+
+
+void createRooms(int section) {
+   int sec_X = 0;
+   int sec_Z = 0;
+
+   switch (section)
+   {
+      case 1:
+         sec_X = (WORLDX / 3);
+         sec_Z = (WORLDZ / 3);
+         break;
+      case 2:
+         sec_X = 2 * (WORLDX / 3);
+         sec_Z = (WORLDZ / 3);
+         break;
+      case 3:
+         sec_X = 3 * (WORLDX / 3);
+         sec_Z = (WORLDZ / 3);
+         break;
+      case 4:
+         sec_X = (WORLDX / 3);
+         sec_Z = 2 * (WORLDZ / 3);
+         break;
+      case 5:
+         sec_X = 2 * (WORLDX / 3);
+         sec_Z = 2 * (WORLDZ / 3);
+         break;
+      case 6:
+         sec_X = 3 * (WORLDX / 3);
+         sec_Z = 2 * (WORLDZ / 3);
+         break;
+      case 7:
+         sec_X = (WORLDX / 3);
+         sec_Z = 3 * (WORLDZ / 3);
+         break;
+      case 8:
+         sec_X = 2 * (WORLDX / 3);
+         sec_Z = 3 * (WORLDZ / 3);
+         break;
+      case 9:
+         sec_X = 3 * (WORLDX / 3);
+         sec_Z = 3 * (WORLDZ / 3);
+         break;
+      default:
+         return;
+         break;
+   }
+
+   for(int i = sec_X - (WORLDX / 3); i <= sec_X; i++) {
+      for(int j = sec_Z - (WORLDZ / 3); j <= sec_Z; j++) {
+         world[i][30][j] = section;
+      }
+   }
+   
+   int room_X = getRandom(5, (WORLDX / 3) - 6);
+   int room_Z = getRandom(5, (WORLDZ / 3) - 6);
+   int corner_X = getRandom(sec_X - (WORLDX / 3) + 2, sec_X - 2 - room_X);
+   int corner_Z = getRandom(sec_Z - (WORLDZ / 3) + 2, sec_Z - 2 - room_Z);
+
+   for(int i = corner_X; i <= corner_X + room_X; i++) {
+      world[i][31][corner_Z] = 6;
+      world[i][31][corner_Z + room_Z] = 6;
+   }
+   for(int j = corner_Z; j <= corner_Z + room_Z; j++) {
+      world[corner_X][31][j] = 6;
+      world[corner_X + room_X][31][j] = 6;
+   }
+
+   printf("Section %d\n", section);
+   printf("Section spot: (%d, %d)\n", sec_X, sec_Z);
+   printf("Room size: %d x %d\n", room_X, room_Z);
+   printf("Corner coords: (%d, %d)\n\n", corner_X, corner_Z);
+}
+
+
 int main(int argc, char** argv) {
    int i, j, k;
 	/* initialize the graphics system */
@@ -385,14 +467,18 @@ int main(int argc, char** argv) {
 
       for(i = 0; i < WORLDX; i++) {
          for(j = 0; j < WORLDZ; j++) {
-            world[i][40][j] = 3;
+            world[i][30][j] = 3;
          }
       }
 
-      world[29][41][39] = 1;
-      world[29][42][40] = 1;
-      world[30][43][40] = 1;
-      world[30][44][39] = 1;
+      world[29][31][39] = 1;
+      world[29][32][40] = 1;
+      world[30][33][40] = 1;
+      world[30][34][39] = 1;
+
+      for(int i = 1; i < 10; i++) {
+         createRooms(i);
+      }
    }
 
 
