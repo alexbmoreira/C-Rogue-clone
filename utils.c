b95daa0366057d764edf631f46ba4c69776d7ae1
alexbmoreira <alexbmoreira@gmail.com>
2021-01-27 12:15:24 -0500

    Head-on collisions can't see into blocks

--- utils.c
+++ utils.c
@@ -5,3 +5,18 @@ int getRandom(int min, int max) {
    int r = (rand() % (max + 1 - min)) + min;
    return r;
 }
+
+
+float roundFloat(float round_me) {
+   round_me += 0.05;
+   // float ten = 10;
+   // float by_ten = round_me * 10;
+   float rounded = roundf(round_me*10.0f)/10.0f;
+   // float rounded = r_by_ten / ten;
+
+   if(rounded <= 0.001 && rounded >= -0.001) {
+      rounded += (rounded < 0) ? -0.1: 0.1;
+   }
+
+   return rounded;
+}
