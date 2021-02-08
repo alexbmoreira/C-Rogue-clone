b9ba2c9b2bd64f6767a72f75d08221ebaf128439
alexbmoreira <alexbmoreira@gmail.com>
2021-02-08 17:01:45 -0500

    Made climbing a little better

--- a1.c
+++ a1.c
@@ -125,10 +125,12 @@ void collisionResponse() {
       // printf("diff_z: %f\n", diff_z);
       // printf("rounded diff_z: %f\n\n", roundFloat(diff_z));
 
-      int int_next_x = (int)(next_x - append_x)*(-1);
+      int int_next_x = (int)(next_x)*(-1);
+      int int_predicted_x = (int)(next_x - append_x)*(-1);
       int int_next_y = (int)next_y*(-1) - 1;
       int int_next_y_head = (int)next_y*(-1);
-      int int_next_z = (int)(next_z - append_z)*(-1);
+      int int_next_z = (int)(next_z)*(-1);
+      int int_predicted_z = (int)(next_z - append_z)*(-1);
 
       int int_curr_x = (int)curr_x*(-1);
       int int_curr_y = (int)curr_y*(-1) - 1;
@@ -140,7 +142,7 @@ void collisionResponse() {
       // printf("Next: %f, %f, %f\n", next_x, next_y, next_z);
       // printf("Next Int: %d, %d, %d\n", int_next_x, int_next_y, int_next_z);
 
-      if(world[int_next_x][int_next_y][int_next_z] != 0 || world[int_next_x][int_next_y_head][int_next_z] != 0) {
+      if(world[int_next_x][int_next_y][int_next_z] != 0 || world[int_predicted_x][int_next_y_head][int_predicted_z] != 0) {
          float gotoX = curr_x;
          float gotoY = curr_y;
          float gotoZ = curr_z;
@@ -170,6 +172,62 @@ void collisionResponse() {
 
          setViewPosition(gotoX, gotoY, gotoZ);
       }
+
+      // float currX, currY, currZ;
+      // float nextX, nextY, nextZ;
+
+      // getOldViewPosition(&currX, &currY, &currZ);
+      // getViewPosition(&nextX, &nextY, &nextZ);
+
+      // float diff_x = curr_x - next_x;
+      // float diff_z = curr_z - next_z;
+
+      // float append_x = (diff_x < 0) ? -0.25: 0.25;
+      // float append_z = (diff_z < 0) ? -0.25: 0.25;
+
+      // int int_nextX = (int)(next_x - append_x)*(-1);
+      // int int_nextY = (int)nextY*(-1);
+      // int int_nextZ = (int)(next_z - append_z)*(-1);
+
+      // int int_currX = (int)currX*(-1);
+      // int int_currY = (int)currY*(-1);
+      // int int_currZ = (int)currZ*(-1);
+
+      // // printf("Current: %f, %f, %f\n", currX, currY, currZ);
+      // // printf("Current Int: %d, %d, %d\n", int_currX, int_currY, int_currZ);
+      // // printf("Next: %f, %f, %f\n", nextX, nextY, nextZ);
+      // // printf("Next Int: %d, %d, %d\n", int_nextX, int_nextY, int_nextZ);
+
+      // if(world[int_nextX][int_nextY][int_nextZ] != 0) {
+      //    float gotoX = currX;
+      //    float gotoY = currY;
+      //    float gotoZ = currZ;
+
+      //    if(world[int_currX][int_nextY][int_currZ] != 0) {
+      //       gotoX = nextX;
+      //       gotoZ = nextZ;
+      //    }
+      //    if(world[int_currX][int_currY][int_nextZ] != 0) {
+      //       if(world[int_currX][int_currY + 1][int_nextZ] == 0) {
+      //          gotoZ = nextZ;
+      //          gotoY = currY - 1;
+      //       }
+      //       else {
+      //          gotoZ = currZ;
+      //       }
+      //    }
+      //    if(world[int_nextX][int_currY][int_currZ] != 0) {
+      //       if(world[int_nextX][int_currY + 1][int_currZ] == 0) {
+      //          gotoX = nextX;
+      //          gotoY = currY - 1;
+      //       }
+      //       else {
+      //          gotoX = currX;
+      //       }
+      //    }
+
+      //    setViewPosition(gotoX, gotoY, gotoZ);
+      // }
    }
 }
 
