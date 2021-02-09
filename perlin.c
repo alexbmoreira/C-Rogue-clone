27d84edb07ba085646f3507016c46df5f503b319
alexbmoreira <alexbmoreira@gmail.com>
2021-02-09 12:47:26 -0500

    Restyled code

--- perlin.c
+++ perlin.c
@@ -1,5 +1,6 @@
 #include "perlin.h"
 #include "graphics.h"
+#include "utils.h"
 
 /* 
     Code for generating 2D Perlin noise inspired by pseudocode found here:
@@ -18,10 +19,10 @@ float noise[WORLDX][WORLDZ];
 float terrain[WORLDX][WORLDZ];
 
 float Noise(int x, int y) {
-  int n = x + y * 57;
-  n = (n << 13) ^ n;
-  int t = (n * (n * n * 362489573 + 946737083) + 1376312589) & 0x7fffffff;
-  return 1.0 - ((float)t / 1073741824.0);
+    int n = x + y * 57;
+    n = (n << 13) ^ n;
+    int t = (n * (n * n * 362489573 + 946737083) + 1376312589) & 0x7fffffff;
+    return 1.0 - ((float)t / 1073741824.0);
 }
 
 float SmoothedNoise(int x, int y) {
@@ -37,30 +38,33 @@ float Interpolate(float a, float b, float x) {
 }
 
 float InterpolatedNoise(float x, float y) {
-  int integer_X = x;
-  float fractional_X = x - integer_X;
-  int integer_Y = y;
-  float fractional_Y = y - integer_Y;
+    int integer_X = x;
+    float fractional_X = x - integer_X;
+    int integer_Y = y;
+    float fractional_Y = y - integer_Y;
 
-  float v1 = SmoothedNoise(integer_X, integer_Y),
-         v2 = SmoothedNoise(integer_X + 1, integer_Y),
-         v3 = SmoothedNoise(integer_X, integer_Y + 1),
-         v4 = SmoothedNoise(integer_X + 1, integer_Y + 1),
-         i1 = Interpolate(v1, v2, fractional_X),
-         i2 = Interpolate(v3, v4, fractional_X);
-  return Interpolate(i1, i2, fractional_Y);
+    float v1 = SmoothedNoise(integer_X, integer_Y),
+    float v2 = SmoothedNoise(integer_X + 1, integer_Y),
+    float v3 = SmoothedNoise(integer_X, integer_Y + 1),
+    float v4 = SmoothedNoise(integer_X + 1, integer_Y + 1),
+    float i1 = Interpolate(v1, v2, fractional_X),
+    float i2 = Interpolate(v3, v4, fractional_X);
+
+    return Interpolate(i1, i2, fractional_Y);
 }
 
 float ValueNoise_2D(float x, float y) {
-  float total = 0,
-         frequency = pow(2, numOctaves),
-         amplitude = 1;
-  for (int i = 0; i < numOctaves; i++) {
-    frequency /= 2;
-    amplitude *= persistence;
-    total += InterpolatedNoise(x / frequency, y / frequency) * amplitude;
-  }
-  return total / frequency;
+    float total = 0,
+    float frequency = pow(2, numOctaves),
+    float amplitude = 1;
+
+    for (int i = 0; i < numOctaves; i++) {
+        frequency /= 2;
+        amplitude *= persistence;
+        total += InterpolatedNoise(x / frequency, y / frequency) * amplitude;
+    }
+
+    return total / frequency;
 }
 
 void generateNoise() {
