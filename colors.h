a1f0e2f78e64aa507f102237413263b8e23d4919
alexbmoreira <alexbmoreira@gmail.com>
2021-02-11 13:33:20 -0500

    Named colors for dungeon stuff

---
+++ colors.h
@@ -0,0 +1,10 @@
+
+#define CLR_U_STAIR 1
+#define CLR_BLACK 2
+#define CLR_D_STAIR 3
+#define CLR_DG_WALL 4
+#define CLR_DG_FLOOR 5
+#define CLR_GRASS 6
+#define CLR_DIRT 7
+#define CLR_SNOW 8
+#define CLR_CLOUDS 9
